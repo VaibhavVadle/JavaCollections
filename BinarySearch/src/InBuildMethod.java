a1f86fc57306679ef51c8df26079ecec166f76b6
VaibhavVadle <vaibhavvadle09@gmail.com>
2024-11-14 00:22:56 +0530

    first commit

---
+++ BinarySearch/src/InBuildMethod.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class InBuildMethod {
+    public static void main(String[] args)
+    {
+
+        int a[] = { 10, 20, 15, 22, 35 };
+
+        // Sorting the above array
+        // using sort() method of Arrays class
+        Arrays.sort(a);
+
+        int x = 22;
+
+        int res = Arrays.binarySearch(a, x);
+
+        if (res >= 0)
+            System.out.println(x + " found at index = " + res);
+        else
+            System.out.println(x + " Not found");
+
+        x = 40;
+        res = Arrays.binarySearch(a, x);
+
+        if (res >= 0)
+            System.out.println(x + " found at index = " + res);
+        else
+            System.out.println(x + " Not found");
+    }
+}
