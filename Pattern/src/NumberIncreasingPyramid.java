6efcfd16d6eaee6e2a412eefd797f94ec0179bc9
VaibhavVadle <vaibhavvadle09@gmail.com>
2024-11-18 22:45:42 +0530

    Print pattern - Number Triangle, Increasing pyramid & hollow square

---
+++ Pattern/src/NumberIncreasingPyramid.java
@@ -0,0 +1,27 @@
+import java.util .*;
+public class NumberIncreasingPyramid {
+// Java Program to print pattern
+// Number-increasing pyramid
+
+    // Function to demonstrate pattern
+    public static void printPattern(int n) {
+        int i, j;
+        // outer loop to handle number of rows
+        for (i = 1; i <= n; i++) {
+            // inner loop to handle number of columns
+            for (j = 1; j <= i; j++) {
+                // printing column values upto the row
+                // value.
+                System.out.print(j + " ");
+            }
+            // print new line for each row
+            System.out.println();
+        }
+    }
+
+    // Driver Function
+    public static void main(String args[]) {
+        int n = 6;
+        printPattern(n);
+    }
+}
